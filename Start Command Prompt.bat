4af749654a6eace709316090395dd6e9a84be80e
Rod Hinman <rod@auroraresearch.com>
2025-01-10 15:34:57 -1000

    Added comments I should have included before.

--- Start Command Prompt.bat
+++ Start Command Prompt.bat
@@ -1,3 +1,7 @@
+:: Batch file to change to the proper folder and set up the environment.
+:: This file assumes that python poetry is on your computer and you have already
+:: run "poetry install" to create a virtual environment for the project.
+:: The FRC2025 repository should be on your desktop.
 @echo off
 echo ===================================================================================
 echo ==== Setup batch file for FRC2025 ====
@@ -10,9 +14,14 @@ echo    For more details about updating and deploying, see the docs folder.
 echo =
 echo ===================================================================================
 
+:: Change to the proper directory, the git repository for the FRC2025 code.
 cd %USERPROFILE%\Desktop\FRC2025
+
+:: Activate the virtual environment set up for robotpy.
 poetry env activate > temp.bat
 call temp.bat
 del temp.bat
+
+:: Stop in a command prompt.
 cmd /k
 @echo on
