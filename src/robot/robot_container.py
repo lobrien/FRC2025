235e2d88a79494af2372cb7263f67878aea8e4c1
Larry O'Brien <lobrien@knowing.net>
2025-01-26 11:15:50 -1000

    RobotContainer comments
    Signed-off-by: Larry O'Brien <lobrien@knowing.net>

--- src/robot/robot_container.py
+++ src/robot/robot_container.py
@@ -12,6 +12,20 @@ from commands.drive_forward_command import DriveForwardCommand
 from commands.drive_with_joystick_command import DriveWithJoystickCommand
 from commands.turn_to_angle_command import TurnToAngleCommand
 
+# The `RobotContainer` class is where the robot's structure and behavior are defined.
+# It is the glue that holds the robot together.
+# Hardware-wise:
+# * The `RobotContainer` has group of Subsystems. When you add or delete a complete
+#   subsystem, you will need to update the `RobotContainer`.
+# * The `RobotContainer` has one or more input controllers (e.g. Xbox controllers, joysticks).
+#
+# Command-wise:
+# * The `RobotContainer` instantiates the Commands and CommandGroups that make
+#   up the robot's behavior. These commands are created and wired together here
+#  in the `RobotContainer`. If you ever want to review the robot's behavior, the
+#  `RobotContainer` is the proper place to look. Even the autonomous and default commands
+#   should be instantiated here.
+#* The `RobotContainer` maps between the input devices and the Commands that operate on the Subsystems.
 class RobotContainer:
     def __init__(self):
         self.drive_subsystem = DriveSubsystem() #Update the array when ready
@@ -21,7 +35,6 @@ class RobotContainer:
         self.teleop_command = TurnToAngleCommand(self.drive_subsystem, should_set_fn=self.set_45_degrees)
         
         # DriveWithJoystickCommand(self.drive_subsystem, driving_percent=self.get_drive_value_from_joystick) #The teleop command is the drive with joystick commmand which takes the drive subsystem and the getting drive value function
-                
 
         self.controller.a().onTrue(PrintSomethingCommand("WHEA A Button Pressed"))
         self.controller.b().onTrue(TurnToAngleCommand(self.drive_subsystem, lambda: True))
