d40e5f0f28bd7ed8f5a241e2a7d1ed3d886645db
Larry O'Brien <lobrien@knowing.net>
2025-01-26 11:05:59 -1000

    `Robot` class comments and full lifecycle
    No behavior added.
    
    Signed-off-by: Larry O'Brien <lobrien@knowing.net>

--- src/robot/robot.py
+++ src/robot/robot.py
@@ -8,19 +8,60 @@ import robot_container
 from subsystems import drive_subsystem
 from constants.operatorinterfaceconstants import OperatorInterfaceConstants
 
-# Robot Class
+# The `Robot` class is the main robot class. It is responsible for
+# the robot lifecycle methods. Because this is a command-based robot,
+# a lot of the robot lifecycle is handled by the `CommandScheduler`
+# and the majority of lifecycle methods are empty (`pass`).
+#
+# The `Robot` class *has-a* `RobotContainer`. The `RobotContainer` is
+# responsible for customizing the robot.
+#
+###### CAUTION! CAUTION! ###### CAUTION! CAUTION! ######
+# This class is unlikely to require modification.
 class Robot(commands2.TimedCommandRobot):
     def robotInit(self):
         self.container = robot_container.RobotContainer()
 
-    ###### CAUTION! CAUTION! ###### CAUTION! CAUTION! ######
-    # Do not change anything beneath this line during debugging.
-    # It is important to keep the robot periodic and autonomous init methods as they are.
+    # Robot overall lifecycle methods
+    def robotInit(self):
+        pass
 
     def robotPeriodic(self):
         CommandScheduler.getInstance().run()
 
+    def robotEnd(self):
+        pass
+
+    # Autonomous lifecycle methods
     def autonomousInit(self):
         self.autonomous_command = self.container.get_auto_command()
         if self.autonomous_command is not None:
             self.autonomous_command.schedule()
+
+    def autonomousPeriodic(self):
+        pass
+
+    def autonomousEnd(self):
+        pass
+
+    # Teleop lifecycle methods
+    def teleopInit(self):
+        if self.autonomous_command is not None:
+            self.autonomous_command.cancel()
+
+    def teleopPeriodic(self):
+        pass
+
+    def teleopEnd(self):
+        pass
+
+    # Test lifecycle methods
+    def testInit(self):
+        # Cancel all running commands
+        CommandScheduler.getInstance().cancelAll();
+
+    def testPeriodic(self):
+        pass
+
+    def testEnd(self):
+        pass
